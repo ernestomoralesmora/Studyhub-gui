56b27b9b20a51f6484f36500995b4a277cfd30e9
ernestomoralesmora <ernestomoralesmora7@gmail.com>
2026-05-14 19:05:02 +0200

    Clean repository and update ignored generated files

--- parts/copy-files/run/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-source /home/ernesto_7/Escritorio/studyhub-gui/parts/copy-files/run/environment.sh
-set -x
-cp --archive --link --no-dereference . "/home/ernesto_7/Escritorio/studyhub-gui/parts/copy-files/install"
